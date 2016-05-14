c63262bf757cf4cfacc0c194cf6aee10959e227a
unknown <1079160717@qq.com>
2016-05-14 17:58:22 +0800

    all

--- js/detail.js
+++ js/detail.js
@@ -2,7 +2,7 @@ $(function(){
     var $scroll = $('#scroll');
     var $scrollLiLength = $scroll.find('li').length;
     var $scrollLiWidth = $scroll.find('li').eq(0).width();
-    $('#scroll').width($scrollLiLength * ($scrollLiWidth + 10) -10);
+    $('#scroll').width($scrollLiLength * ($scrollLiWidth + 10));
     var scrollX = new IScroll('#scroll-wraper',{
         scrollX: true,
         scrollY: false
